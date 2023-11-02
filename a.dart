1c911975963d787bf80787665be976edbb05146f
Desh Deepak Kant <118960904+DeshDeepakKant@users.noreply.github.com>
2023-11-02 21:51:19 +0530

    anya

---
+++ a.dart
@@ -0,0 +1,24 @@
+body: RichText(
+    text: TextSpan(
+        style: TextStyle(
+          color: Colors.blue,
+          fontSize: 22,
+        ),
+        children: <TextSpan>[
+      TextSpan(text: 'Hello'),
+      TextSpan(
+          text: 'Anya',
+          style: TextStyle(
+            fontSize: 44,
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          )),
+      TextSpan(
+          text: 'Flutter',
+          style: TextStyle(
+            fontSize: 61,
+            color: Colors.lightGreen,
+            fontWeight: FontWeight.bold,
+          ))
+    ]))
